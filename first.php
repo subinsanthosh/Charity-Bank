4b5768bc0f441dcc3d320d3617117fb94c934488
subinsanthosh <108916618+subinsanthosh@users.noreply.github.com>
2022-07-14 22:18:47 +0530

    Add files via upload

---
+++ first.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="first.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
+</head> 
+<body>
+    <header>
+        
+    <div class="row">
+     <img src="images/z.jpg" alt="">
+        <div class="logo">
+          <h2>⠀CHARITY BANK</h2>
+        </div>
+            
+
+        
+    </div>
+        
+    <div class="hero">
+      <h1>Small Efforts</h1>
+      <p>Make Big Changes.</p>
+      <div class="more">
+        <h1>Volunteers do not necessarily have the time;</h1>
+        <p>they just have the heart...</p>
+      </div>
+      <div class="button">
+         <a href="login.php" class="btn btn-one"> Proceed</a>  
+      </div>    
+            
+    </div>
+    
+    </header>
+
+
+        
+</body>    
+</html>
